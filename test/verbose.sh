7df38542787699ce07ee922d0698fd489fb84e81
Victor Engmark <victor.engmark@gmail.com>
2012-06-13 18:31:59 +0200

    Extracted `verbose_echo` function to separate script including tests

---
+++ test/verbose.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+. "$(dirname -- "$0")"/../verbose_echo.sh
+verbose=
+verbose_echo 'message'
---
+++ verbose_echo.sh
@@ -0,0 +1,38 @@
+# NAME
+#        verbose_echo.sh - Function to print a message only in verbose mode
+#
+# SYNOPSIS
+#        . verbose_echo.sh
+#        verbose_echo [OPTION...] MESSAGE...
+#
+# DESCRIPTION
+#        Prints messages to standard output if $verbose is set. Options are
+#        forwarded to `echo`.
+#
+# BUGS
+#        https://github.com/l0b0/shell-includes/issues
+#
+# COPYRIGHT AND LICENSE
+#        Copyright (C) 2010-2012 Victor Engmark
+#
+#        This program is free software: you can redistribute it and/or modify it
+#        under the terms of the GNU General Public License as published by the
+#        Free Software Foundation, either version 3 of the License, or (at your
+#        option) any later version.
+#
+#        This program is distributed in the hope that it will be useful, but
+#        WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#        General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License along
+#        with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+verbose_echo() {
+    if [ -n "${verbose+defined}" ]
+    then
+        echo "$@"
+    fi
+}
